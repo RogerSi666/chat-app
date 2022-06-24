f0058e8ad645b083ff7bcd01f25f9a24ffd4b1ae
RogerSi666 <107162778+RogerSi666@users.noreply.github.com>
2022-06-24 12:46:57 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 end
 def create
@@ -11,6 +12,7 @@ def create
   if @message.save
     redirect_to room_messages_path(@room)
   else
+    @messages = @room.messages.includes(:user)
     render :index
   end
 end
